6db7aaa9fd10a6a42177e5847b4237cb8999a857
regorDam <regorm88@gmail.com>
2016-11-06 19:46:31 +0100

    GameManager Singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour 
 {
+	private static GameManager instance;
+
+	public static GameManager Instance
+	{ get {return instance; }}
+
 
 	// Use this for initialization
 	void Awake()
 	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			DestroyObject (gameObject);
+
+		DontDestroyOnLoad ();
+
 		FileManager.Instance.GenerateGame ();	
 	}
 
